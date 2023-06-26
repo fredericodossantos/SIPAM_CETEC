22b80a5d3d98770b64227679ba8bb7c4ab99cc28
fredericodossantos <frederico.dos.santos@gmail.com>
2023-06-26 18:45:29 -0300

    first commit

---
+++ dashboard/edit_equip.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+// Use the database.php file
+include_once '../db/database.php';
+
+if (isset($_POST['update_data'])) {
+    $name = $_POST['name'];
+    $description = $_POST['description'];
+    $category = $_POST['category'];
+    $id = $_POST['id']; 
+    
+    $sql = "UPDATE equipment SET name=?, description=?, category=? WHERE id=?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sssi", $name, $description, $category, $id);
+    $result = $stmt->execute();
+    
+    if ($result) {
+        $_SESSION['status'] = "Equipamento atualizado com sucesso";
+        header("Location: manage_equipment.php");
+        exit();
+    } else {
+        $_SESSION['status'] = "Equipamento não atualizado";
+        header("Location: form_edit_equip.php?id=" . $id); // Redirect back to the form with the ID parameter
+        exit();
+    }
+    
+    $stmt->close();
+    $conn->close();
+}
+?>
